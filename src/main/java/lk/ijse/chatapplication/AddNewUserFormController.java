72203359bea98a3d5a88afa1deaf8ccc26e5e5d7
Parami <paramiashinsana@gmail.com>
2023-07-10 12:55:53 +0530

    initialize the project

---
+++ src/main/java/lk/ijse/chatapplication/AddNewUserFormController.java
@@ -0,0 +1,111 @@
+package lk.ijse.chatapplication;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.chatapplication.Model.ClientModel;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AddNewUserFormController extends Application implements Initializable {
+    public JFXTextField txtUsername;
+    static String username;
+    public AnchorPane root;
+    public JFXPasswordField txtPassword;
+    public JFXComboBox comUsername;
+    public ArrayList<String> arrayList = new ArrayList<>();
+
+    public void btnAddOnAction(ActionEvent actionEvent) throws SQLException, IOException {
+        String user_name = (String) comUsername.getSelectionModel().getSelectedItem();
+        String password = ClientModel.searchByUser_Name(user_name);
+        username = user_name;
+
+        String id = ClientModel.searchByUser_ID(user_name);
+        if (searchArray(id)) {
+            if (password.equalsIgnoreCase(txtPassword.getText())) {
+                Stage stage = new Stage();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/ClientForm.fxml"))));
+                stage.setTitle(user_name + "'s Chat");
+                stage.show();
+                arrayList.add(id);
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Incorrect Password !").show();
+            }
+        }else {
+            new Alert(Alert.AlertType.ERROR, "Already used !").show();
+        }
+        txtPassword.clear();
+    }
+
+    public void btnRegisterOnMouseCliced(MouseEvent mouseEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/RegisterForm.fxml"))));
+        stage.setTitle("Register Form");
+        stage.show();
+        root.getScene().getWindow().hide();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            loadUserNames();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/AddNewUserForm.fxml"))));
+        stage.show();
+
+        new Thread(()->{
+            Server server = new Server();
+            try {
+                server.Server();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public boolean searchArray(String username){
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).equalsIgnoreCase(username)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void loadUserNames() throws SQLException {
+        try{
+            List<String> username = ClientModel.getUserName();
+            ObservableList<String> obList = FXCollections.observableArrayList();
+
+            for (String un : username){
+                obList.add(un);
+            }
+            comUsername.setItems(obList);
+        }catch (SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "SQL Error !!").show();
+        }
+    }
+}
---
+++ src/main/java/lk/ijse/chatapplication/ClientFormController.java
@@ -0,0 +1,242 @@
+package lk.ijse.chatapplication;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientFormController extends Thread{
+    private final String[] emojis = {
+            "\uD83D\uDE00", // 😀
+            "\uD83D\uDE01", // 😁
+            "\uD83D\uDE02", // 😂
+            "\uD83D\uDE03", // 🤣
+            "\uD83D\uDE04", // 😄
+            "\uD83D\uDE05", // 😅
+            "\uD83D\uDE06", // 😆
+            "\uD83D\uDE07", // 😇
+            "\uD83D\uDE09", // 😉
+            "\uD83D\uDE0A", // 😊
+            "\uD83D\uDE0B", // 😋
+            "\uD83D\uDE0C", // 😌
+            "\uD83D\uDE0D", // 😍
+            "\uD83D\uDE0E", // 😎
+            "\uD83D\uDE0F", // 😏
+            "\uD83D\uDE13"  // 😓
+    };
+
+
+    public TextField txtClient;
+    public VBox vBox;
+    public Label lblClientName;
+    public AnchorPane emojiPane;
+    public GridPane emojiGridpane;
+    PrintWriter writer;
+
+    BufferedReader reader;
+    Socket socket;
+
+    private FileChooser fileChooser;
+    private File filePath;
+
+    public void initialize(){
+        emojiLoad();
+        emojiPane.setVisible(false);
+        lblClientName.setText(AddNewUserFormController.username);
+        try {
+            socket = new Socket("localhost",5000);
+            System.out.println("Socket is connected with server!");
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            this.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void emojiLoad(){
+        int buttonIndex = 0;
+        for (int row = 0; row < 4; row++) {
+            for (int column = 0; column < 4; column++) {
+                if (buttonIndex < emojis.length) {
+                    String emoji = emojis[buttonIndex];
+                    JFXButton emojiButton = createEmojiButton(emoji);
+                    emojiGridpane.add(emojiButton, column, row);
+                    buttonIndex++;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private JFXButton createEmojiButton(String emoji) {
+        JFXButton button = new JFXButton(emoji);
+        button.getStyleClass().add("emoji-button");
+        button.setOnAction(this::emojiButtonAction);
+        button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        GridPane.setFillWidth(button, true);
+        GridPane.setFillHeight(button, true);
+        button.setStyle("-fx-font-size: 25; -fx-text-fill: black; -fx-background-color: #F0F0F0; -fx-border-radius: 50");
+        return button;
+    }
+
+    private void emojiButtonAction(ActionEvent event) {
+        JFXButton button = (JFXButton) event.getSource();
+        txtClient.appendText(button.getText());
+    }
+
+    @Override
+    public void run(){
+        try {
+            while (true) {
+                String msg = reader.readLine();
+                String[] tokens = msg.split(" ");
+                String cmd = tokens[0];
+                System.out.println(cmd);
+
+                StringBuilder fullMsg = new StringBuilder();
+                for (int i = 1; i < tokens.length; i++) {
+                    fullMsg.append(tokens[i]+" ");
+                }
+
+                String[] msgToAr = msg.split(" ");
+                String st = "";
+                for (int i = 0; i < msgToAr.length - 1; i++) {
+                    st += msgToAr[i + 1] + " ";
+                }
+
+                Text text = new Text(st);
+                String firstChars = "";
+                if (st.length() > 3) {
+                    firstChars = st.substring(0, 3);
+                }
+
+                if (firstChars.equalsIgnoreCase("img")) {
+                    //for the Images
+                    st = st.substring(3, st.length() - 1);
+
+                    File file = new File(st);
+                    Image image = new Image(file.toURI().toString());
+
+                    ImageView imageView = new ImageView(image);
+
+                    imageView.setFitHeight(150);
+                    imageView.setFitWidth(200);
+
+                    HBox hBox = new HBox(10);
+                    hBox.setAlignment(Pos.BOTTOM_RIGHT);
+
+                    if (!cmd.equalsIgnoreCase(lblClientName.getText())) {
+                        vBox.setAlignment(Pos.TOP_LEFT);
+                        hBox.setAlignment(Pos.CENTER_LEFT);
+
+                        Text text1 = new Text("  " + cmd + " :");
+                        hBox.getChildren().add(text1);
+                        hBox.getChildren().add(imageView);
+                        hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+
+                    } else {
+                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+                        hBox.getChildren().add(imageView);
+                        Text text1 = new Text(": Me ");
+                        hBox.getChildren().add(text1);
+                        hBox.setStyle("-fx-alignment: center-right;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+                    }
+
+                    Platform.runLater(() -> vBox.getChildren().addAll(hBox));
+
+
+                }else {
+                    TextFlow tempFlow = new TextFlow();
+
+                    if (!cmd.equalsIgnoreCase(lblClientName.getText() + ":")) {
+                        Text txtName = new Text(cmd + " ");
+                        txtName.getStyleClass().add("txtName");
+                        tempFlow.getChildren().add(txtName);
+                    }
+
+                    tempFlow.getChildren().add(text);
+                    tempFlow.setMaxWidth(200); //200
+
+                    TextFlow flow = new TextFlow(tempFlow);
+
+                    HBox hBox = new HBox(12); //12
+
+                    if (!cmd.equalsIgnoreCase(lblClientName.getText() + ":")) {
+                        vBox.setAlignment(Pos.TOP_LEFT);
+                        hBox.setAlignment(Pos.CENTER_LEFT);
+                        hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+                        flow.setStyle("-fx-background-color:   white;-fx-background-radius:15;-fx-font-size: 15;-fx-font-weight: normal;-fx-text-fill: black;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                        hBox.getChildren().add(flow);
+                    } else {
+                        Text text2 = new Text(fullMsg + ": Me");
+                        TextFlow flow2 = new TextFlow(text2);
+                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+                        hBox.setStyle("-fx-alignment: center-right;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+                        flow2.setStyle("-fx-background-color:  #CCFF9A;-fx-background-radius:15;-fx-font-size: 15;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                        hBox.getChildren().add(flow2);
+                    }
+                    Platform.runLater(() -> vBox.getChildren().addAll(hBox));
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void send(){
+        String msg = txtClient.getText();
+        writer.println(lblClientName.getText() + ": " + msg);
+        System.out.println(lblClientName.getText() + ": " + msg);
+        txtClient.clear();
+
+        if(msg.equalsIgnoreCase("BYE") || (msg.equalsIgnoreCase("logout"))) {
+            System.exit(0);
+
+        }
+    }
+
+    public void OnActionClientSend(ActionEvent actionEvent) {
+        send();
+    }
+
+    public void txtClientOnAction(ActionEvent actionEvent) {
+        send();
+    }
+
+    public void ImageOnMouseClicked(MouseEvent mouseEvent) {
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image");
+        this.filePath = fileChooser.showOpenDialog(stage);
+        writer.println(lblClientName.getText() + " " + "img" + filePath.getPath());
+    }
+
+    public void btnEmojiOnMouseClicked(MouseEvent mouseEvent) {
+        emojiPane.setVisible(true);
+    }
+
+    public void btnOnMouseExited(MouseEvent mouseEvent) {
+        emojiPane.setVisible(false);
+    }
+}
